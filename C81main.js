435f193cabeb9c0fc97cfea17102a494c18e81a1
DyaweJhonson <92857513+DyaweJhonson@users.noreply.github.com>
2021-12-13 17:44:32 +0530

    Add files via upload

---
+++ C81main.js
@@ -0,0 +1,39 @@
+Canvas=document.getElementById("myCanvas");
+color = "blue";
+
+ctx = Canvas.getContext("2d");
+ctx.beginPath();
+ctx.strokeStyle = color;
+ctx.lineWidth = 3;
+ctx.arc(200,200,40,0,2* Math.PI);
+ctx.stroke();
+
+
+
+
+Canvas.addEventListener("mousedown",my_mousedown);
+
+function my_mousedown(e)
+
+{
+
+   mouse_x = e.clientX-Canvas.offsetLeft;
+   mouse_y = e.clientY-Canvas.offsetTop;
+
+   console.log("X=" + mouse_x +",Y = " +mouse_y);
+
+   circle(mouse_x,mouse_y);
+}
+
+
+
+function circle(mouse_x,mouse_y)
+{
+
+    ctx.beginPath();
+    ctx.strokeStyle = color;
+    ctx.lineWidth = 3;
+    ctx.arc(mouse_x,mouse_y,40,0,2* Math.PI);
+    ctx.stroke();
+
+}
